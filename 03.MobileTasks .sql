62090ddb1f56405d192588617699e5ff0f7c2c64
FaridAlasgarli05 <ferid.elesgerli200510@gmail.com>
2026-04-15 15:56:25 +0400

    Mobile Operator layihə - Tapşırıqlar

---
+++ 03.MobileTasks .sql
@@ -0,0 +1,68 @@
+-- 1. Abonentlərin son 3 ayda istifadə etdiyi xidmətlərin adlarını və istifadə sayını göstər:
+Ekrana abonentin adı, soyadı, xidmətin adı və istifadə sayı çıxsın.
+      
+--2.Hər abonent üçün son ödəniş tarixini və məbləğini göstər: 
+--    Ekrana Ad, soyad, son ödəniş tarixi, məbləği və ödənişin üsulu haqqında informasiyalar çıxsın.
+            
+-- 3.Aktiv xidmətlər üzrə abonentlərin sayını və ümumi ödəniş məbləğini göstər:
+--    Ekrana xidmətin adı,abonentlərin sayını və ümumi ödəniş məbləği haqqında informasiyalar çıxsın. 
+     
+-- 4.Hər abonent üçün edilən zənglərin sayını və ümumi zəng müddətini göstər:
+--    Ekrana ad, soyad, abonent üçün edilən zənglərin sayını, ümumi zəng müddətini, zəngin tipi haqqında informasiyalar çıxsın.
+ 
+--5.Hər abonent üçün tarifə görə aylıq ödədikləri məbləği və zənglərin sayını göstər:
+--    Ekrana ad, soyad,tarifə görə aylıq ödədikləri məbləğ və zənglərin sayı haqqında informasiyalar çıxsın. 
+         
+--6.Zənglərin növünə görə abonentlərin sayını və ümumi zəng müddətini göstər:
+            
+--7. Aktiv olmayan, lakin son 6 ayda ödəniş etmiş abonentləri tap.
+Ekrana ad, soyad, status və ödəniş tarixi çıxsın.
+              
+--8.Son 6 ayda edilən ödənişlərin məbləğini və xidmətlərin sayını abonentlər üzrə göstər.
+--    Ekrana abonentin adı, abonentin soyadı, xidmətlərin sayı və 6 ayda edilən ödənişlərin məbləği haqqında informasiyalar çıxsın.
+     
+--9.Abonentlərdən ən çox şikayət edən 10 nəfərin adını və şikayət sayını göstər:
+Ekrana ad, soyad və ümumi şikayət sayı çıxsın.
+ 
+--10. Hər abonent üçün son 12 ayda göndərilən SMS-lərin sayını və SMS məzmununu göstər:
+     
+--11.Hər abonent üçün edilən zənglərin ümumi müddətini və ödəniş məlumatlarını göstər:    
+--   Ekrana abonentin adı, abonentin soyadı, zənglərin ümumi müddətini ödəniş məlumatları haqqında informasiyalar çıxsın. 
+      
+--12. Hər tarif üzrə abonentlərin aylıq ödədikləri məbləğin orta dəyərini və zənglərin sayını göstər:
+--   Ekrana tarif adı, abonentlərin aylıq ödədikləri məbləğin orta dəyəri və zənglərin sayı haqqında informasiyalar çıxsın. 
+
+--13. Hər abonent üçün son 12 ayda göndərilən SMS-lərin məzmununu və göndərilən SMS növlərini göstər:
+--   Ekrana abonentin adı, soyadı, son 12 ayda göndərilən SMS-lərin məzmununu və göndərilən SMS növləri haqqında informasiyalar çıxsın.  
+      
+-- 14. Hər abonent üçün son 6 ayda edilən ödənişlərin məbləğini və xidmətlərin sayını göstər:
+--   Ekrana abonentin adı, soyadı, son 6 ayda edilən ödənişlərin məbləğini və xidmətlərin sayı haqqında informasiyalar çıxsın.
+       
+-- 15. Hər abonent üçün ödənişlər və şikayətlərin məbləğini göstər:
+--   Ekrana abonentin adı, soyadı, hər abonent üçün ödənişlər və şikayətlərin sayı haqqında informasiyalar çıxsın. 
+      
+-- 16. Abonentlərin yaş qrupuna görə və cinsiyyətə görə bölgüsünü göstər:
+--    Yaş qrupları aşağıdakı şəkildədi
+--    18 və aşağı
+--    19-30
+--    31-50
+--    51 və yuxarı
+--    Ekrana yaş aralığı,cins və say haqqında informasiyalar çıxsın.   
+       
+-- 17. Hər abonentin ümumi ödədiyi məbləği və onların ödədikləri məbləğin tariflərin ortalama ödəmə məbləğindən yüksək olub 
+--     olmadığını göstərən sorğu:
+       
+-- 18. Hər abonentin zənglərin ümumi müddətini və onların zəng müddətinin abonentin yaş qrupunun ortalama zəng müddətindən 
+--     yüksək olub olmadığını göstərən sorğu:             
+   
+
+      
+
+
+      
+
+     
+
+
+
+
